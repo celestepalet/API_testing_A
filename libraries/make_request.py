8a56128f5bdd8c72d31eff2eca878326b66dda3b
Fabian <cabrejoariasfabian@gmail.com>
2023-04-17 16:05:33 -0500

    bring new develop

--- libraries/make_request.py
+++ libraries/make_request.py
@@ -1,16 +1,3 @@
-#
-# @make_request.py Copyright (c) 2023 Jalasoft.
-# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
-#
-# All rights reserved.
-#
-# This software is the confidential and proprietary information of
-# Jalasoft, ("Confidential Information"). You shall not
-# disclose such Confidential Information and shall use it only in
-# accordance with the terms of the license agreement you entered into
-# with Jalasoft.
-#
-
 import requests
 from libraries.get_url import GetUrl
 
