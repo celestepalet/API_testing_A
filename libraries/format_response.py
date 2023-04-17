5adcec87b04376bcc6d7e34a54ca2a763bf9527a
celestepalet <celestepalet@gmail.com>
2023-04-17 16:43:01 -0300

    add TC and libraries for put method

--- libraries/format_response.py
+++ libraries/format_response.py
@@ -1,33 +1,48 @@
+#
+# @format_response.py Copyright (c) 2023 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+#
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 from lxml import html, etree
 import json
-from libraries.make_request import MakeRequest
-from libraries.validation import Validation
 
 
 class FormatResponse:
-    def __init__(self):
-        pass
+    """Gives format to a request response"""
 
     def format_json(self, response):
+        """Gives json format to a request response"""
         response = json.loads(response.content)
         return response
 
     def format_str(self, response):
+        """Gives text format to a request response"""
         response = response.text
         return response
 
     def format_xml(self, response):
+        """Gives xml format to a request response"""
         response = etree.fromstring(response.content)
         return etree.tostring(response, pretty_print=True)
-        pass
 
     def format_html(self, response):
+        """Gives html format to a request response"""
         response = html.fromstring(response.content)
         return html.tostring(response, pretty_print=True)
 
-    def get_format_response(self, response, response_type='format_text', **kwargs):
+    def get_format_response(self, response, response_type='format_text'):
+        """Instances the method with tha required format"""
         response_with_format = dic_response_type[response_type]
         return response_with_format(response)
+
 
 dic_response_type = {
     "format_json": FormatResponse().format_json,
--- libraries/get_url.py
+++ libraries/get_url.py
@@ -1,20 +1,27 @@
+#
+# @get_url.py Copyright (c) 2023 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+#
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 import os
-from dotenv import load_dotenv
-from requests.auth import HTTPBasicAuth
-load_dotenv()
 
 
 class GetUrl:
-    def __init__(self):
-        pass
+    """Gets the url"""
 
     def get_base_url(self):
+        """Get the base url of the API from .env file"""
         return os.getenv("URL")
 
-    def get_complete_url(self, path, id=None, **kwargs):
+    def get_complete_url(self, path, id='', **kwargs):
+        """Get the endpoint url"""
         url = '%s/%s/%s' % (GetUrl().get_base_url(), path, id)
         return url
-
-def get_auth(user, passw):
-    basic = HTTPBasicAuth(user, passw)
-    return basic
