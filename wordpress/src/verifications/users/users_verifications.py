022e5e2a5cafed61fbdeaa1d31e7ca5a8ac67384
celestepalet <111649534+celestepalet@users.noreply.github.com>
2023-04-21 03:28:23 -0300

    firsts e2e scenarios, users endpoint (#20)

--- wordpress/src/verifications/users/users_verifications.py
+++ wordpress/src/verifications/users/users_verifications.py
@@ -26,4 +26,15 @@ class UsersVerification:
         logger.info(f'Actual result: {actual}')
         logger.info(f'The deleted user id: {value}')
 
+    def verify_value_edited(self, actual, actual_value, old, old_value):
+        """Verify that the actual result does not have a specific value"""
+        logger.info(f'The past email: {old_value}')
+        logger.info(f'The actual email: {old_value}')
+        logger.info(f'Past result: {actual}')
+        logger.info(f'Actual result: {actual}')
+        assert_that(actual).does_not_contain_value(old)
+        assert_that(actual).contains_value(actual_value)
+
+
+
 
