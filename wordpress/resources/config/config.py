7f20966dab9cbcf25c384c4f72256eff53e86093
celestepalet <celestepalet@gmail.com>
2023-04-26 01:32:44 -0300

    commit for pull

--- wordpress/resources/config/config.py
+++ wordpress/resources/config/config.py
@@ -1,6 +1,17 @@
 import os
 
-username = os.getenv("USER")
-password = os.getenv("PASSWORD")
-url = os.getenv("URL")
-url_extension = os.getenv("URL_EXTENSION")
+
+class Config:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            cls._instance = cls()
+        return cls._instance
+
+    def __init__(self):
+        self.username = os.getenv("USER")
+        self.password = os.getenv("PASSWORD")
+        self.url = os.getenv("URL")
+        self.url_extension = os.getenv("URL_EXTENSION")
