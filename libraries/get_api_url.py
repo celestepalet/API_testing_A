77ba6233678f7b3d2271ae86815dac5acdb60624
celestepalet <celestepalet@gmail.com>
2023-04-27 11:55:16 -0300

    change config file

--- libraries/get_api_url.py
+++ libraries/get_api_url.py
@@ -1,12 +1,10 @@
-from wordpress.resources.config.config import Config
-
-config = Config.get_instance()
+from wordpress.resources.config.config import url, url_extension
 
 
 class GetApiUrl:
     """Gets the urlof the endpoint"""
 
-    def get_complete_url(self, endpoint, id='', **kwargs):
+    def get_complete_url(self, endpoint, id=''):
         """Gets the endpoint url"""
-        endpoint_url = '%s/%s/%s/%s' % (config.url, config.url_extension, endpoint, id)
+        endpoint_url = '%s/%s/%s/%s' % (url, url_extension, endpoint, id)
         return endpoint_url
